b12a0da78ffc26e78306f18218512ec3b41ae030
Leandro Avila - L3anAv <leandroav.dev@gmail.com>
2024-05-14 15:00:53 -0300

    Ruteo con switch nuevamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import {useRoute} from "wouter";
+import {Switch, Route} from "wouter";
 import Home  from "./pages/home/home"
 import Editor from './pages/editor/editor'
 import Route404 from "./components/404Route/404"
 import {RellenarStoreConFijos} from './utils/inciarlizarApp'
-import {useBrowserLocation} from 'wouter/use-browser-location'
 
 function App() {
-
-  const [location] = useBrowserLocation();
 
   RellenarStoreConFijos()
   console.disableYellowBox = true;
   
-  if (location === '/editor') {
-    return <Editor />;
-  }else if(location === '/'){
-    return <Home />;
-  }else{
-    return <Route404 />;
-  }
+  return(
+  <Switch>
+    <Route path="/" component={Home}/>
+    <Route path="/editor" component={Editor}/>
+
+    <Route><Route404/></Route>
+  </Switch>
+  )
 }
 
 export default App
